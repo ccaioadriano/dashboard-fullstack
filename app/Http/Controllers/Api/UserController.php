d6fc6fbbded010407ccfdd1ebe99697d18ea4360
ccaioadriano <caiobhadriano@gmail.com>
2024-12-26 19:08:46 -0300

    feat: implement soft deletes for users and improve user form submission handling

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -15,7 +15,7 @@ class UserController extends Controller
      */
     public function index()
     {
-        return UserResource::collection(User::query()->orderBy('id', 'desc')->paginate(10));
+        return UserResource::collection(User::query()->where('deleted_at', '=', null )->orderBy('id', 'desc')->paginate(10));
     }
 
     /**
